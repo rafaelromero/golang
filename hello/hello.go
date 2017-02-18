bec3ab97bf4323bccd35dbff10ffa49b14fffc1f
rafael <rafael@rafaelromero.info>
2017-02-18 10:16:25 -0600

    add string util :

--- hello/hello.go
+++ hello/hello.go
@@ -1,10 +1,15 @@
 package main
 
-import "fmt"
+import (
+		"fmt"
+		"github.com/rafaelromero/golang/stringutil"
+	)
 
 func main(){
 
 
 	fmt.Printf("purple haze, @  all in my braiin!\n")
-
+	fmt.Printf("REVERSE:\n")
+	fmt.Printf(stringutil.Reverse("purple haze, @ all in my braiinx!"))
+	fmt.Printf("\n")	
 }
