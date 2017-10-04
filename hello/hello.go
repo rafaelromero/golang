00f6898a9653aca75c1c77b3ea0976abe4b5ed84
rafael <rafael@rafaelromero.info>
2017-10-04 09:59:13 -0500

    saving

--- hello/hello.go
+++ hello/hello.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 
-	fmt.Printf("purple haze, @  all in my braiin!\n")
+	fmt.Printf("ppurple haze, @  all in my braiin!\n")
 	fmt.Printf("REVERSE:\n")
 	fmt.Printf(stringutil.Reverse("purple haze, @ all in my braiinx!"))
 	fmt.Printf("\n")
