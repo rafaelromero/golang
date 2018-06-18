885aa2a71c5f344746137312e24a47eab8da1aa7
Rafael Romero <rafael@rafaelromero.info>
2018-06-18 17:47:37 -0500

    slices exercise

--- slices/slices.go
+++ slices/slices.go
@@ -5,21 +5,8 @@ import "fmt"
 func main(){
 
 
-
-	s:=make([]string, 4)
-	s[0] = "first string"
-	s[1] = "second string"
-	s[2] = "3"
-	s[3] = "4"
-
-	fmt.Println("array " , s)
-	slice1 := s[1:3]
-	fmt.Println(slice1)
-
-
-	i:=make([]int, 2)
-	i[0] = 5200
-	fmt.Println("int arry ", i)
+     stars:=[6]int{1,2,3,4,5,6}
 	
-
+     //build a slice and print it out
+     fmt.Println(stars[1:3])
 }
