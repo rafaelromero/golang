7c224d86c00be6c6ca6dd87c7c95beffa484ab8b
= <rafael@rafaelromero.info>
2019-08-08 02:29:37 -0500

    cleaning up helllo script

--- hello/hello.go
+++ hello/hello.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/rafaelromero/golang/stringutil"
 )
 
 func main() {
 
-	fmt.Printf("ppurple haze, @  all in my braiin!\n")
-	fmt.Printf("REVERSE:\n")
-	fmt.Printf(stringutil.Reverse("purple haze, @ all in my braiinx!"))
-	fmt.Printf("\n")
+	fmt.Printf("hello world!\n")
+	//fmt.Printf("\n")
 }
